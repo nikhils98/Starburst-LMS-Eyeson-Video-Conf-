3c56e4e86825df4420ad88dbbb46988328e2956e
Nikhil Satiani <nikhil.satiani@gmail.com>
2020-05-01 22:42:41 +0500

    updated directory structure, added a template and bootstrap files

--- Application/__init__.py
+++ Application/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('home.html')
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from Application import app
+from application import app
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
+else:
+    app.run()
