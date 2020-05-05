4333a817bce89f622df2e13fe22c8c0af1a3c0b0
NikhilS98 <nikhil.satiani@gmail.com>
2020-05-05 14:03:23 +0500

    work in progress assignment submission

--- Application/assignment_submissions.py
+++ Application/assignment_submissions.py
@@ -9,3 +9,30 @@ import os
 ASSIGNMENT_UPLOAD_FOLDER = 'assignments'
 PROJECT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files', str(org.orgId))
 
+@app.route('/assignmentSubmission')
+@authenticate
+def assignmentSubmission():
+    return render_template('')
+
+@app.route('/downloadSubmission')
+@authenticate
+def downloadSubmission():
+    # logic for downloading
+
+    # after successful download
+    return redirect('assignmentSubmission')
+
+@app.route('/submitAssignment', methods=["GET", "POST"])
+@authenticate
+def submit_assignment():
+    if request.method == "GET":
+        return render_template('submit_assignment_modal.html')
+    # logic for submission
+    formData = request.form
+
+    submission = models.AssignmentSubmission()
+
+
+    # after successful submission
+    flash("Assignment Submitted")
+    return redirect('assignmentSubmission')
